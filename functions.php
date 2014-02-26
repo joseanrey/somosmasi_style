b10af21777c1467330e981850c48a1b51401b4db
joseanrey <jose.an.rey@gmail.com>
2014-02-26 19:41:01 +0100

    Subo la primera versión del tema somosmasi para Trending

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/* Función para conocer si una página es subpagina de otra */
+
+function is_subpage() {
+	global $post;                              // load details about this page
+		if ( is_page() && $post->post_parent ) {   // test to see if the page has a parent
+	        return $post->post_parent;             // return the ID of the parent post
+		} 
+		else {                                   // there is no parent so ...	
+        	return false;                          // ... the answer to the question is false
+		}
+}
+?>
